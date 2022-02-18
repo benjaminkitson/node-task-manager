578ebe9e950cdd1d83bf54ca4d7d3e516300b2bc
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-02-18 16:31:45 +0000

    adding various elements to index.js for frontend

--- src/index.js
+++ src/index.js
@@ -5,9 +5,20 @@ const Task = require('./models/task')
 const validator = require('validator')
 const userRouter = require('./routers/users')
 const taskRouter = require('./routers/tasks')
+const path = require('path')
+
+function pathGen(pathString) {
+  return path.join(__dirname, pathString)
+}
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.set('view engine', 'hbs');
+app.set(pathGen('../templates/views'))
+hbs.registerPartials(pathGen('../templates/partials'))
+
+app.use(express.static(pathGen('../public')))
 
 app.use(express.json());
 app.use(userRouter)
